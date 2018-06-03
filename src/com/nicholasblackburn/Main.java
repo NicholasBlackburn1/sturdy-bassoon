16137040a88e3ad83f6b4e7a11329728ce76b558
NICKY B <Nickblackburn02@gmail.com>
2018-06-03 10:49:33 -0400

    added main consolemenu

--- src/com/nicholasblackburn/Main.java
+++ src/com/nicholasblackburn/Main.java
@@ -10,10 +10,6 @@ import java.util.Scanner;
 
 public class Main {
     public int Main(String[] args) throws IOException {
-        System.out.print("enter 1 for text to hex or enter 2 for hex to binary");
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String s = br.readLine();
 
         int menu;
 
@@ -24,13 +20,22 @@ public class Main {
 
             System.out.println("Choose from these choices");
             System.out.println("-------------------------\n");
-            System.out.println("1 - texttohex");
-            System.out.println("2 - Encrypt a number");
-            System.out.println("3 - Decrypt a number");
+            System.out.println("1 - text to hex");
+            System.out.println("2 - hex to binary");
+            System.out.println("3 - help");
             System.out.println("4 - Quit");
 
             selection = input.nextInt();
-            return selection;
+        if (selection == 1){
+            texttohex.main(args);
+        }
+        if (selection == 2){
+            hextobinary.main(args);
+
+         }
+        return selection;
+
+
         }
     }
 
